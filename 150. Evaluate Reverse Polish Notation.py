ed85e4d409fb7c47852558a076503b5cde2d323e
ffekirnew <ffekirnew0808@gmail.com>
2022-09-05 03:00:11 +0300

    Modified the comments to be a bit more descriptive

--- 150. Evaluate Reverse Polish Notation.py
+++ 150. Evaluate Reverse Polish Notation.py
@@ -4,9 +4,9 @@ class Solution:
         if len(tokens) == 1:
             return int(tokens[0])
         else:
-            numbers = []
-            results = []
-            stack = [] # this is a list to keep track of which numbers to combine using the tokens and in which order
+            numbers = [] # this is a stack to keep pure numbers as they come in from the tokens list
+            results = [] # this is a stack to keep the result of combined numbers
+            stack = [] # this is a stack to keep track of which numbers to combine using the tokens and in which order
             for index in range(len(tokens)):
                 token = tokens[index]
                 try: # the int(token) will throw an error that will need to be handled, i handle that with the except since that is the only exception to be thrown in the try block, string.isnumeric() was not doing the job for some reason
